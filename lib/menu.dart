098119584d0d79f682fe43f704dfa79178568ae7
diego-mascarenhas <diego.mascarenhas@icloud.com>
2022-12-18 00:01:03 +0100

    Logo

--- lib/menu.dart
+++ lib/menu.dart
@@ -14,105 +14,112 @@ class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Drawer(
         child: SingleChildScrollView(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildHeader(context),
-            buildMenuItems(context),
-          ],
-        )),
-      );
-
-  Widget buildHeader(BuildContext context) => Container(
-        padding: EdgeInsets.only(
-          top: MediaQuery.of(context).padding.top,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ],
+          ),
         ),
       );
 
+  Widget buildHeader(BuildContext context) => Container(
+        color: Colors.white,
+        padding: EdgeInsets.only(
+          top: 8 + MediaQuery.of(context).padding.top,
+          bottom: 4,
+          left: 22,
+          right: 36,
+        ),
+        child: const Image(image: AssetImage('assets/images/logo.png')),
+      );
+
   Widget buildMenuItems(BuildContext context) => Container(
-      padding: const EdgeInsets.all(12),
-      child: Wrap(
-        runSpacing: 8,
-        children: [
-          ListTile(
-            leading: const Icon(Icons.dashboard_outlined),
-            title: const Text('Dashboard'),
-            onTap: () =>
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => const DashboardPage(),
-            )),
-          ),
-          ListTile(
-            leading: const Icon(Icons.group_outlined),
-            title: const Text('Contactos'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const ContactosPage(),
-              ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.forward_to_inbox_outlined),
-            title: const Text('Comunicaciones'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const ComunicacionesPage(),
-              ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.monitor_outlined),
-            title: const Text('Landings'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const LandingsPage(),
-              ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.perm_media_outlined),
-            title: const Text('Multimedia'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const MultimediaPage(),
-              ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.send_outlined),
-            title: const Text('Mailer'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const MailerPage(),
-              ));
-            },
-          ),
-          const Divider(color: Colors.black54),
-          ListTile(
-            leading: const Icon(Icons.settings_suggest_outlined),
-            title: const Text('Configuración'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const ConfiguracionPage(),
-              ));
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout_outlined),
-            title: const Text('Logout'),
-            onTap: () {
-              Navigator.pop(context);
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const LoginPage(),
-              ));
-            },
-          ),
-        ],
-      ));
+        padding: const EdgeInsets.all(12),
+        child: Wrap(
+          runSpacing: 8,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.dashboard_outlined),
+              title: const Text('Dashboard'),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const DashboardPage(),
+              )),
+            ),
+            ListTile(
+              leading: const Icon(Icons.group_outlined),
+              title: const Text('Contactos'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const ContactosPage(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.forward_to_inbox_outlined),
+              title: const Text('Comunicaciones'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const ComunicacionesPage(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.monitor_outlined),
+              title: const Text('Landings'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const LandingsPage(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.perm_media_outlined),
+              title: const Text('Multimedia'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const MultimediaPage(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.send_outlined),
+              title: const Text('Mailer'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const MailerPage(),
+                ));
+              },
+            ),
+            const Divider(color: Colors.black54),
+            ListTile(
+              leading: const Icon(Icons.settings_suggest_outlined),
+              title: const Text('Configuración'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const ConfiguracionPage(),
+                ));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout_outlined),
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ));
+              },
+            ),
+          ],
+        ),
+      );
 }
