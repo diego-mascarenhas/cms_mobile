79785199c9bfab19128e786a57b4e2430f6ead73
diego-mascarenhas <diego.mascarenhas@icloud.com>
2022-12-19 14:46:40 +0100

    Listado de SMTPs

--- lib/data/smtps.dart
+++ lib/data/smtps.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import '../models/smtpModel.dart';
+import 'package:cms_mobile/models/smtpModel.dart';
 
 import 'package:http/http.dart' as http;
 
--- lib/pages/mailer_smtps_page.dart
+++ lib/pages/mailer_smtps_page.dart
@@ -1,43 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:cms_mobile/menu.dart';
-
-import 'package:cms_mobile/data/smtps.dart';
 import 'package:cms_mobile/models/smtpModel.dart';
+import 'package:cms_mobile/data/smtps.dart';
 
 import 'package:http/http.dart' as http;
 
-class SmtpsPage extends StatefulWidget {
+class SmtpsPage extends StatelessWidget {
   const SmtpsPage({Key? key}) : super(key: key);
 
   @override
-  State<SmtpsPage> createState() => _SmtpsPageState();
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('SMTPs'),
+        ),
+        body: FutureBuilder<List<Smtp>>(
+          future: fetchItems(http.Client()),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text('An error has occurred!'),
+              );
+            } else if (snapshot.hasData) {
+              return ContactosList(Item: snapshot.data!);
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(color: Color(0xFFed5565)),
+              );
+            }
+          },
+        ),
+      );
 }
 
-class _SmtpsPageState extends State<SmtpsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SMTPs'),
-      ),
-      drawer: const NavigationDrawer(),
-    );
-  }
-}
+class ContactosList extends StatelessWidget {
+  const ContactosList({super.key, required this.Item});
 
-class SmtpsList extends StatelessWidget {
-  const SmtpsList({super.key, required this.item});
-
-  final List<Smtp> item;
+  final List<Smtp> Item;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: item.length,
+      itemCount: Item.length,
       itemBuilder: (context, index) {
         return ListTile(
-          title: Text(item[index].host),
-          subtitle: Text("Mails en Cola: " + item[index].mailq.toString()),
+          title: Text(Item[index].host),
+          subtitle: Text('ID: ' + Item[index].id.toString()),
         );
       },
     );
