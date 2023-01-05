9eecff2a263fb65f7e4bda016907e479a99eaf57
diego-mascarenhas <diego.mascarenhas@icloud.com>
2023-01-05 18:33:32 +0100

    Pantalla de Login Protrait

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,7 +1,11 @@
+import 'package:cms_mobile/pages/contactos_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cms_mobile/menu.dart';
 import 'package:cms_mobile/pages/login_page.dart';
+import 'package:cms_mobile/pages/comunicaciones_page.dart';
+
+enum MenuItem { item1, item2, item3 }
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -31,6 +35,41 @@ class _DashboardPageState extends State<DashboardPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dashboard'),
+        //backgroundColor: Colors.transparent,
+        //elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_active),
+            onPressed: () {},
+          ),
+          PopupMenuButton<MenuItem>(
+              onSelected: (value) {
+                if (value == MenuItem.item1) {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ContactosPage()));
+                } else if (value == MenuItem.item2) {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ComunicacionesPage()));
+                } else if (value == MenuItem.item3) {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ComunicacionesPage()));
+                }
+              },
+              itemBuilder: (context) => const [
+                    PopupMenuItem(
+                      value: MenuItem.item1,
+                      child: Text('Servicios'),
+                    ),
+                    PopupMenuItem(
+                      value: MenuItem.item2,
+                      child: Text('Mis Facturas'),
+                    ),
+                    PopupMenuItem(
+                      value: MenuItem.item3,
+                      child: Text('Crear Ticket'),
+                    ),
+                  ])
+        ],
       ),
       drawer: const NavigationDrawer(),
       body: SafeArea(
@@ -39,16 +78,16 @@ class _DashboardPageState extends State<DashboardPage> {
           child: Center(
             child: Column(
               children: [
-                Text(
+                const Text(
                   "Bienvenido!",
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
                 Text(
                   "Token: $token",
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 15,
                 ),
                 OutlinedButton.icon(
