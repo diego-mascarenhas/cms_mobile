27227d6e1f8c870b36edb4b602b7ef131adacde1
diego-mascarenhas <diego.mascarenhas@icloud.com>
2023-02-15 19:00:50 +0100

    Fijar el tamaño de la tipografía

--- lib/pages/trade_signals_page.dart
+++ lib/pages/trade_signals_page.dart
@@ -44,17 +44,28 @@ class TradeSignalsList extends StatelessWidget {
       itemBuilder: (context, index) {
         return ListTile(
           contentPadding: const EdgeInsets.all(15),
-          title: Text(item[index].triger,
-              style: TextStyle(
-                  color: (item[index].side == 'sell')
-                      ? Colors.red
-                      : Colors.green)),
+          title: Text(
+            item[index].triger,
+            style: TextStyle(
+                color: (item[index].side == 'sell') ? Colors.red : Colors.green,
+                fontSize: 18 * MediaQuery.textScaleFactorOf(context)),
+          ),
           subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('${item[index].signal} (${item[index].symbol})'),
-              Text(DateFormat('dd/MM/yyyy HH:mm:ss')
-                  .format(DateTime.parse(item[index].fecha_alta).toLocal())),
+              Text(
+                '${item[index].signal} (${item[index].symbol})',
+                style: TextStyle(
+                    fontSize: 16 * MediaQuery.textScaleFactorOf(context),
+                    height: 1.2),
+              ),
+              Text(
+                DateFormat('dd/MM/yyyy HH:mm:ss')
+                    .format(DateTime.parse(item[index].fecha_alta).toLocal()),
+                style: TextStyle(
+                    fontSize: 12 * MediaQuery.textScaleFactorOf(context),
+                    height: 1.4),
+              ),
             ],
           ),
           leading: CircleAvatar(
